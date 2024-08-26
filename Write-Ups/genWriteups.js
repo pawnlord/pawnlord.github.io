e14ec7510deacf587ac7eb3fab53ee010d6354f1
pawnlord <scruffduff321@gmail.com>
2024-08-26 14:24:46 -0400

    Fix syntax highlighting issue with prismjs

--- Write-Ups/genWriteups.js
+++ Write-Ups/genWriteups.js
@@ -1,22 +1,20 @@
+import * as prism from "/prism.js"
 
 let writeupNames = ["TCP1P", "Buckeye", "CursedCTF", "umdctf", "sekaiCTF"];
 
 
 
-
-
-async function replaceInstances(name){
+export async function replaceInstances(name){
     var text = await (await fetch(name + ".md")).text();
-    console.log(text)
     var converter = new showdown.Converter(),
         html      = converter.makeHtml(text);
         
     var replace = document.getElementById("content-replace-me");
     replace.innerHTML = html;
-    console.log(html)
+    Prism.highlightAll();
 }
 
-function replaceHeader() {
+export function replaceHeader() {
     var replace = document.getElementById("header-replace-me");
     var innerHTML = "<div id=\"header-list\">\n"; 
     for(var i in writeupNames) {
@@ -24,12 +22,11 @@ function replaceHeader() {
         innerHTML += "<a href=\"" + name +".html\" id=\"button-link\" class=\"button-link-hover\">" + name + "</a></br>\n";
     }
     innerHTML += "</div>\n"
-    console.log(innerHTML)
     replace.innerHTML = innerHTML;
 
 }
 
-function replaceList() {
+export function replaceList() {
     var replace = document.getElementById("list-replace-me");
     var innerHTML = "<ul id=\"content\">\n"; 
     for(var i in writeupNames) {
@@ -37,6 +34,5 @@ function replaceList() {
         innerHTML += "<li><a href=\"" + name +".html\" id=\"button-link\" class=\"button-link-hover\">" + name + "</a></li>\n";
     }
     innerHTML += "</divul\n"
-    console.log(innerHTML)
     replace.innerHTML = innerHTML;
 }
